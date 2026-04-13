70c13ad62c6bf3b04388134352f709d41d4d1bba
joewilko123 <joe10w@icloud.com>
2026-04-13 18:53:15 +0100

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
     const session = await stripe.checkout.sessions.create({
       mode: priceId.includes('lifetime') ? 'payment' : 'subscription',
-      payment_method_types: ['card', 'apple_pay', 'google_pay'],
+      payment_method_types: ['card'],
       line_items: [
         {
           price: priceId,
